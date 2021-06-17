69988b741c307a293d6f5fc83f218c60754b3e7c
Zarmina <zarabbas7@gmail.com>
2021-06-17 18:43:00 +0200

    Update UserServiceImpl.java

--- src/main/java/io/swagger/service/UserServiceImpl.java
+++ src/main/java/io/swagger/service/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package io.swagger.service;
 
 import io.swagger.service.interfaces.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService
 {
 }

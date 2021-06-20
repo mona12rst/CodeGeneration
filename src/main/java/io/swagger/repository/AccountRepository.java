3406c89282f4d1bd2e8d951a17173f269c91e387
Junayeth <54932574+Junayeth@users.noreply.github.com>
2021-06-20 15:45:20 +0200

    Add files via upload

--- src/main/java/io/swagger/repository/AccountRepository.java
+++ src/main/java/io/swagger/repository/AccountRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends CrudRepository<Account, AccountPK>
 {
+    Account findAccountByIBAN(String iban);
+
 }

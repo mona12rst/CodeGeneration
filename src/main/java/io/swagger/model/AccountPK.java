a98ee19aab570c59550cf61706afc5dc34b5850c
Mona Rostami <64336157+mona12rst@users.noreply.github.com>
2021-06-24 19:24:04 +0200

    fixed small issues

--- src/main/java/io/swagger/model/AccountPK.java
+++ src/main/java/io/swagger/model/AccountPK.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 package io.swagger.model;
 
 import lombok.*;
@@ -36,42 +36,4 @@ public class AccountPK implements Serializable
     }
 
 }
-=======
-package io.swagger.model;
 
-import lombok.*;
-
-import java.io.Serializable;
-import java.util.Objects;
-
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-public class AccountPK implements Serializable
-{
-    private String IBAN;
-    private long accountId;
-
-    @Override
-    public boolean equals(Object o) {
-        if ( this == o ) {
-            return true;
-        }
-        if ( o == null || getClass() != o.getClass() ) {
-            return false;
-        }
-        AccountPK pk = (AccountPK) o;
-        return Objects.equals( IBAN, pk.IBAN ) &&
-                Objects.equals( accountId, pk.accountId );
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash( IBAN, accountId );
-    }
-
-}
->>>>>>> Stashed changes
--- src/main/java/io/swagger/service/interfaces/AccountService.java
+++ src/main/java/io/swagger/service/interfaces/AccountService.java
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 package io.swagger.service.interfaces;
 
 import io.swagger.model.Account;
@@ -24,30 +24,4 @@ public interface AccountService
 
 
 }
-=======
-package io.swagger.service.interfaces;
 
-import io.swagger.model.Account;
-import io.swagger.model.Balance;
-import io.swagger.model.DTO.AccountDTO;
-import io.swagger.model.IBAN;
-
-import java.util.List;
-
-public interface AccountService
-{
-    List<Account> getAllAccounts();
-
-    Account getAccountByIban(String iban);
-
-    int deleteAccountByIban(String iban);
-
-    Account editAccountByAccountByIban(String iban, AccountDTO accountDTO);
-
-    Account createAccount(AccountDTO accountDTO);
-
-    Balance getAccountBalanceByIban(String iban);
-
-
-}
->>>>>>> Stashed changes
